1591a81fd3e5b830caa02f34c03c6cf5f541caaf
Jimmeeee <hytonenjimi@gmail.com>
2019-06-13 15:13:20 +0300

    tehtava 4.5

--- blogilista/tests/list_helper.test.js
+++ blogilista/tests/list_helper.test.js
@@ -85,4 +85,17 @@ describe('total likes', () => {
     const result = listHelper.totalLikes(emptyList)
     expect(result).toBe(0)
   })
+
+  test('most likes', () => {
+    const result = listHelper.favouriteBlog(bigList)
+    expect(result).toStrictEqual([{
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    }])
+  })
 })
+
--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -6,10 +6,17 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, order) => sum+order.likes,0)
 }
 
+const favouriteBlog = (blogs) => {
+    var likes = blogs.map(value => value.likes)
+    var maks = Math.max(...likes)
+    //console.log(likes, maks)
+    return blogs.filter(blog => blog.likes===maks)
+}
 
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 
 }
